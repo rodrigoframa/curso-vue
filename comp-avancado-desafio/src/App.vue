0329d33ac4bc30471d40a49aa171b4090646511a
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2018-12-16 00:22:33 -0300

    uso avancado de componentes - desafio resolvido

--- comp-avancado-desafio/src/App.vue
+++ comp-avancado-desafio/src/App.vue
@@ -1,14 +1,18 @@
 <template>
 	<div id="app">
 		<span>
-			<button class="vermelho">Carregar Componente Vermelho</button>
-			<button class="verde">Carregar Componente Verde</button>
-			<button class="azul">Carregar Componente Azul</button>
+			<button class="vermelho" @click="componente = 'Vermelho'">Carregar Componente Vermelho</button>
+			<button class="verde" @click="componente = 'Verde'">Carregar Componente Verde</button>
+			<button class="azul" @click="componente = 'Azul'">Carregar Componente Azul</button>
 		</span>
+		<keep-alive>
+			<component :is="componente">
+				<span slot="conteudo">
+					Conteúdo do Componente <strong>{{ componente }}</strong>
+				</span>
+			</component>
+		</keep-alive>
 		
-		<Vermelho />
-		<Verde />
-		<Azul />
 	</div>
 </template>
 
@@ -20,6 +24,11 @@ import Azul from './components/Azul.vue'
 export default {
 	name: 'app',
 	components: { Vermelho, Verde, Azul },
+	data () {
+		return {
+			componente: 'Vermelho'
+		}
+	}
 }
 </script>
 
