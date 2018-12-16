2a386b4d47de64e990c25b3e03aa9b205e722f90
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2018-12-15 22:49:23 -0300

    uso avancado de componentes - ajustado para slots nomeados

--- citacoes/src/components/Citacao.vue
+++ citacoes/src/components/Citacao.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="citacao">
-        <slot></slot>
+        <slot name="fonte"></slot>
+        <slot name="texto"></slot>
+        <slot name="autor"></slot>
     </div>
 </template>
 
@@ -18,6 +20,6 @@ export default {
         max-width: 450px;
     }
     h1 {
-        color: red;
+        color: red!important;
     }
 </style>
