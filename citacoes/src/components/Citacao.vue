0077ee2a971511be5850733637ddfeab974222eb
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2018-12-15 22:16:14 -0300

    componentes avancados - passando por slot

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h1 {
+        color: red;
+    }
+</style>
